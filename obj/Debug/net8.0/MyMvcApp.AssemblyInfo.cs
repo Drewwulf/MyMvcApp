c430912b4345217f3fde6800f9a53b5a21a2987e
CobtahAcademy08 <cobtahacademy08@gmail.com>
2025-12-07 10:42:13 +0000

    #SCHOOL-60 add page mygroup for student

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0836776b78453379ae33fdcb559d5624299df9c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481b703ac5fab7182d986d3ead74b80d3b9f54ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
