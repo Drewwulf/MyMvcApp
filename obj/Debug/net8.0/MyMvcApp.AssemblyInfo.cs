8439eef50e51112f247fd5c1e5da908608560019
Cobtahacademy10 <Cobtahacademy10@gmail.com>
2025-12-14 09:19:55 +0000

    #School-63 Я збільшив заміну на чорний колір, але повністю ще не доробив.

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05aafffd09cfbed74beaf439c3ac0a4bce76445d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88335b18082696506b4b89e27fbfbe1f01c968f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
