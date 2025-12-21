95d7a37b8ecb8b2403bafdcf1ecf4ced2bc5d497
cobtahacademy08 <cobtahacademy08@gmail.com>
2025-12-21 11:08:03 +0000

    school-70

--- Controllers/DirectionController.cs
+++ Controllers/DirectionController.cs
@@ -14,9 +14,32 @@ namespace MyMvcApp.Controllers
         {
             return View(); // повертає Views/Direction/Index.cshtml
         }
-        public IActionResult Edit()
+        public IActionResult Edit(int id)
         {
-            return View(); // повертає Views/Direction/Edit.cshtml
+            var directionPlace =_context.Directions.Find(id);
+            var modal = new DirectionViewModel
+            {
+                DirectionId = id,
+                DirectionName= directionPlace.DirectionName,
+                DirectionDescription=directionPlace.DirectionDescription
+
+            };
+            return View(modal); // повертає Views/Direction/Edit.cshtml
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+         public IActionResult Edit(DirectionViewModel modal)
+        {
+        
+             var direction = _context.Directions.Find(modal.DirectionId);
+
+
+    direction.DirectionName = modal.DirectionName;
+    direction.DirectionDescription = modal.DirectionDescription;
+
+    _context.SaveChanges();
+
+            return View(modal); // повертає Views/Direction/Edit.cshtml
         }
         public IActionResult Details(int id){
 
--- Models/VivewModels/DirectionViewModel.cs
+++ Models/VivewModels/DirectionViewModel.cs
@@ -2,8 +2,10 @@ namespace MyMvcApp.Models
 {
     public class DirectionViewModel
     {
+        public int DirectionId { get; set; }
         public string DirectionName { get; set; }     
         public string DirectionDescription { get; set; }   
         public List<Direction> directions = new List<Direction>();
     }
 }
+     
