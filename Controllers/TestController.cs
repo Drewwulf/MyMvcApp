9ee11810adbbfeacd37f626a3d9f11796077f8b2
cobtahacademy08 <cobtahacademy08@gmail.com>
2026-01-10 11:55:36 +0200

    #SCHOOL-76 add create back end for page test

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using MyMvcApp.Data;
+using MyMvcApp.Models;
 
 namespace MyMvcApp.Controllers
 {
     public class TestController : Controller
     {
+         private readonly ApplicationDbContext _context;
+
+        public TestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View(); // повертає Views/Test/Index.cshtml
@@ -16,9 +24,34 @@ namespace MyMvcApp.Controllers
         {
             return View(); // повертає Views/Destination/Details.cshtml
         }
+      
         public IActionResult Create()
         {
-            return View(); // повертає Views/Destination/Details.cshtml
+            var allTest = _context.Tests.ToList();
+            var allDirections = _context.Directions.ToList();
+            var model = new TestViewModel{ test = allTest,directions = allDirections};
+
+            return View(model); 
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TestViewModel TestViewModel)
+        {
+            var direction = _context.Directions.Find(TestViewModel.DirectionId);
+
+            var test = new Test
+            {
+                TestName = TestViewModel.Name,
+                TestDescription = TestViewModel.Description,
+                TestDifficualty = TestViewModel.Difficualty,
+                Direction = direction
+            };
+            _context.Tests.Add(test);
+            _context.SaveChanges();
+            var allTest = _context.Tests.ToList();
+            var model = new TestViewModel{ test = allTest};
+
+            return RedirectToAction("Create"); 
         }
     }
 }
--- Migrations/20260110083024_AddTestA.Designer.cs
+++ Migrations/20260110083024_AddTestA.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyMvcApp.Data;
 
@@ -10,9 +11,11 @@ using MyMvcApp.Data;
 namespace MyMvcApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260110083024_AddTestA")]
+    partial class AddTestA
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -78,10 +81,6 @@ namespace MyMvcApp.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("TestDifficualty")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
                     b.Property<string>("TestName")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
@@ -90,7 +89,7 @@ namespace MyMvcApp.Migrations
 
                     b.HasIndex("DirectionId");
 
-                    b.ToTable("Tests");
+                    b.ToTable("Test");
                 });
 
             modelBuilder.Entity("MyMvcApp.Models.Test", b =>
