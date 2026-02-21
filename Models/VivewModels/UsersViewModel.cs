1fdb712a4284f924b628a0fead20f756244220a6
Cobtahacademy07 <Cobtahacademy07@gmail.com>
2026-02-21 12:40:14 +0300

    #school-91 change number(ID) to UserName in TableUsers

--- Models/VivewModels/UsersViewModel.cs
+++ Models/VivewModels/UsersViewModel.cs
@@ -3,6 +3,7 @@ namespace MyMvcApp.Models
 public class UsersViewModel
 {
     public string Id { get; set; }
+    public string UserName { get; set; }
     public string Email { get; set; }   
     public string Role { get; set; }
 }
