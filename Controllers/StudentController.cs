b2b5c20f75fbf5d8d9544517dbc2eec86cc96439
Cobtahacademy08 <cobtahacademy08@gmail.com>
2026-04-28 18:38:56 +0300

    #SCHOOL-106 Створено відображення студентів на /Student/Index

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,14 +1,26 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyMvcApp.Data;
 
 namespace MyMvcApp.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class StudentController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View(); // повертає Views/Student/Index.cshtml
+            var studentslist = _context.Students.ToList();
+
+            return View(studentslist); // повертає Views/Student/Index.cshtml
         }
         public IActionResult Edit()
         {
