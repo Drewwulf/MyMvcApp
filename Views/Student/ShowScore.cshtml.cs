547af6d325af2fae13922a83eda75c33df6a2dc9
Cobtahacademy10 <cobtahacademy10@gmail.com>
2026-06-02 17:44:53 +0300

    #SCHOOL-116-Зробити бекенд до сторінки тестів

---
+++ Views/Student/ShowScore.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyMvcApp.Views.Student
+{
+    public class ShowScoreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
