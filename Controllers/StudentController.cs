35d038b27aee8097f5a811898bd5d244ce0861f5
Cobtahacademy08 <cobtahacademy08@gmail.com>
2026-07-04 10:42:12 +0300

    #SCHOOL-131 finish work on design on page MySchedule

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -162,8 +162,30 @@ namespace MyMvcApp.Controllers
 
         public async Task<IActionResult> MySchedule()
         {
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+            var userid = user.Id;
+            var studentId = _context.Students.Where(s => s.UserId == userid).First().Id;
+            var groups = _context.StudentToGroups
+         .Include(g => g.studyGroup)
+             .ThenInclude(sg => sg.Teachers)
+         .Include(g => g.studyGroup)
+             .ThenInclude(sg => sg.Place)
+         .Include(g => g.student)
+         .Where(g => g.StudentId == studentId)
+         .ToList();
+            var group = groups
+        .Select(g => g.studyGroup)
+        .Distinct()
+        .ToList();
+            var schedules = _context.Schedules.Where(g => g.StudyGroupId == group.First().StudyGroupId).Include(sg => sg.Place).ToList();
+            var viewModel = new SheduleViewModel
+            {
+                Schedules = schedules
+
+            };
+            return View(viewModel); // повертає Views/Destination/Details.cshtml
         }
+        
         public async Task<IActionResult> HistoryTest()
         {
             var user = await _userManager.GetUserAsync(User);
