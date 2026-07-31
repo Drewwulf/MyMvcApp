acea675129e8a4dd214abb3e89abd32b0dbf6e48
t1mar-sk <timurskoblej@gmail.com>
2026-07-31 23:52:30 +0300

    #School-169 Вчинено майже вся робота, залишилось Вчинити скачку та сторінку для того щоб вчитель міг передивитись все що здали учні

--- Models/Documents.cs
+++ Models/Documents.cs
@@ -8,10 +8,7 @@ public class Documents
 
     public string FilePath { get; set; }
 
-    
     public int? HomeworkId { get; set; }
-    public Homework Homework { get; set; }
-
-    public int? HomeworkInfoId { get; set; }
-    public HomeworkInfo HomeworkInfo { get; set; }
+    public Homework? Homework { get; set; }
+    public HomeworkInfo? HomeworkInfo { get; set; }
 }
--- Models/ViewModels/HomeworkViewModel.cs
+++ Models/ViewModels/HomeworkViewModel.cs
@@ -5,7 +5,8 @@ namespace MyMvcApp.Models
         public int HomeworkId { get; set; }
         public int StudentId { get;set; }
         public string HomeworkName { get; set; }     
-        public string HomeworkDescription { get; set; }   
+        public string HomeworkDescription { get; set; }
+        public string? Text { get; set; }
 
         public DateTime StartTime { get; set; }      // час початку
         public DateTime SubmitTime { get; set; }
