4c916bd3fba84555286a314eeba6e2e7833e9a7d
Cobtahacademy08 <cobtahacademy08@gmail.com>
2026-08-01 12:20:05 +0300

    #SCHOOL-171 add student list to teacherkabinet page

---
+++ Models/ViewModels/TeacherStudent.ViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMvcApp.Models.ViewModels
+{
+    public class TeacherStudentViewModel
+    {
+
+        public Teachers teachers { get; set; }
+
+        public List<Students> students { get; set; }
+    }
+}
